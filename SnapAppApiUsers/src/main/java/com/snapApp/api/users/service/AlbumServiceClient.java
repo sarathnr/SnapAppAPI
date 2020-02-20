3d6f04fdc4d292758d9b62a80fad5b2ef3ea164a
sarath <sarathnambradath@gmail.com>
2020-02-21 00:34:10 +0530

    Added Sleuth and Zipkin

--- SnapAppApiUsers/src/main/java/com/snapApp/api/users/service/AlbumServiceClient.java
+++ SnapAppApiUsers/src/main/java/com/snapApp/api/users/service/AlbumServiceClient.java
@@ -3,6 +3,8 @@ package com.snapApp.api.users.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,18 +12,47 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.snapApp.api.users.ui.model.AlbumResponseModel;
 
-@FeignClient(name = "albums-ws",fallback=AlbumsServiceFallback.class)
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
+
+@FeignClient(name = "albums-ws", fallbackFactory = AlbumsFallbackFactory.class)
 public interface AlbumServiceClient {
 
-	@GetMapping("/users/{id}/albumss")
+	@GetMapping("/users/{id}/albums")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
 }
 
 @Component
-class AlbumsServiceFallback implements AlbumServiceClient{
+class AlbumsFallbackFactory implements FallbackFactory<AlbumServiceClient> {
+
+	@Override
+	public AlbumServiceClient create(Throwable cause) {
+		// TODO Auto-generated method stub
+		return new AlbumsServiceClientFallback(cause);
+	}
+
+}
+
+class AlbumsServiceClientFallback implements AlbumServiceClient {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private final Throwable cause;
+
+	public AlbumsServiceClientFallback(Throwable cause) {
+		this.cause = cause;
+	}
 
 	@Override
 	public List<AlbumResponseModel> getAlbums(String id) {
+		// TODO Auto-generated method stub
+
+		if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+			logger.error("404 error took place when getAlbums was called with userId: " + id + ". Error message: "
+					+ cause.getLocalizedMessage());
+		} else {
+			logger.error("Other error took place: " + cause.getLocalizedMessage());
+		}
 		List<AlbumResponseModel> defaultAlbums = new ArrayList<>();
 		AlbumResponseModel arm = new AlbumResponseModel();
 		arm.setAlbumId("1234");
