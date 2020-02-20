ddc99fce744546dacd24d97aea188f2ba4643df0
sarath <sarathnambradath@gmail.com>
2020-02-20 18:39:20 +0530

    Feign client implementation and fall back

--- SnapAppApiUsers/src/main/java/com/snapApp/api/users/SnapAppApiUsersApplication.java
+++ SnapAppApiUsers/src/main/java/com/snapApp/api/users/SnapAppApiUsersApplication.java
@@ -2,6 +2,7 @@ package com.snapApp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,11 +10,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import com.snapApp.api.users.shared.FeignErrorDecoder;
+
 import feign.Logger;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients	
+@EnableCircuitBreaker
 public class SnapAppApiUsersApplication {
 
 	public static void main(String[] args) {
@@ -35,4 +39,9 @@ public class SnapAppApiUsersApplication {
 	Logger.Level feignLoggerLevel(){
 	return Logger.Level.FULL;	
 	}
+	
+	/*
+	 * @Bean public FeignErrorDecoder getFeignErrorDecoder() { return new
+	 * FeignErrorDecoder(); }
+	 */
 }
--- SnapAppApiUsers/src/main/java/com/snapApp/api/users/service/AlbumServiceClient.java
+++ SnapAppApiUsers/src/main/java/com/snapApp/api/users/service/AlbumServiceClient.java
@@ -1,16 +1,35 @@
 package com.snapApp.api.users.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.snapApp.api.users.ui.model.AlbumResponseModel;
 
-@FeignClient(name = "albums-ws")
+@FeignClient(name = "albums-ws",fallback=AlbumsServiceFallback.class)
 public interface AlbumServiceClient {
 
-	@GetMapping("/users/{id}/albums")
+	@GetMapping("/users/{id}/albumss")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+
+@Component
+class AlbumsServiceFallback implements AlbumServiceClient{
+
+	@Override
+	public List<AlbumResponseModel> getAlbums(String id) {
+		List<AlbumResponseModel> defaultAlbums = new ArrayList<>();
+		AlbumResponseModel arm = new AlbumResponseModel();
+		arm.setAlbumId("1234");
+		arm.setUserId("default");
+		arm.setName("default");
+		arm.setDescription("default");
+		defaultAlbums.add(arm);
+		return defaultAlbums;
+	}
+
 }
